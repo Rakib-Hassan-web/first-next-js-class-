3b2d29241693a778a9deefe9e6417d0a91f1a771
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-20 12:01:45 +0600

    hover

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,11 @@
+  "use client";
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
 const Navbar = () => {
+    const pathname = usePathname();
   return (
     <>
     
@@ -16,7 +20,16 @@ const Navbar = () => {
 >
   Home
 </Link>
-            <Link href={'/about'} className='text-2xl font-medium text-black' >About</Link>
+                  <Link
+        href="/about"
+        className={`relative text-2xl font-medium text-black 
+          after:content-[''] after:absolute after:left-0 after:-bottom-1 
+          after:h-[2px] after:bg-black after:transition-all after:duration-300
+          ${pathname === "/" ? "after:w-full" : "after:w-0"}
+        `}
+      >
+        abbj
+      </Link>
             <Link href={'/contact'}  className='text-2xl font-medium text-black'>Contact</Link>
         </ul>
     </div>
