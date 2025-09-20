5081e292e606b9b6845ef18cc2e8606d30655f42
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-20 11:53:27 +0600

    about page com

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='bg-green-400 h-screen w-full  flex justify-center items-center text-5xl text-black font-bold '>This is  Page</div>
+    <div className='bg-green-400 h-screen w-full  flex justify-center items-center text-5xl text-black font-bold '>This is Contact Page</div>
 
   )
 }
