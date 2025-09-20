333d9f2bd515b566554e3af87bb0cfbab2f25096
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-20 11:53:15 +0600

    contact page

---
+++ app/contact/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-green-400 h-screen w-full  flex justify-center items-center text-5xl text-black font-bold '>This is  Page</div>
+
+  )
+}
+
+export default page
