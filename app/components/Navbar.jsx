9bbb50d71e657f11c4548b045adb8b2a4a81eb9d
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-20 11:46:17 +0600

    Navbaar complete

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -5,10 +6,10 @@ const Navbar = () => {
     <>
     
     <div className='bg-pink-400 py-5  w-full'>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contac</li>
+        <ul className='flex items-center justify-center gap-5'>
+            <Link href={'#'} className='text-2xl font-bold text-black' >Home</Link>
+            <Link href={'#'} className='text-2xl font-bold text-black' >About</Link>
+            <Link href={'#'}  className='text-2xl font-bold text-black'>Contact</Link>
         </ul>
     </div>
     
