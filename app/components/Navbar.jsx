41255e6c04e944297d3b64aa3dcf1d9b680b5883
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-20 11:58:57 +0600

    hover

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,8 +6,16 @@ const Navbar = () => {
     <>
     
     <div className='bg-pink-400 py-5  w-full'>
-        <ul className='flex items-center justify-center gap-5'>
-            <Link href={'/'} className='text-2xl font-medium text-black' >Home</Link>
+        <ul className='flex items-center justify-center gap-10'>
+            <Link
+  href="/"
+  className="relative text-2xl font-medium text-black 
+             after:content-[''] after:absolute after:left-0 after:-bottom-1 
+             after:w-0 after:h-[2px] after:bg-black after:transition-all after:duration-300 
+             hover:after:w-full"
+>
+  Home
+</Link>
             <Link href={'/about'} className='text-2xl font-medium text-black' >About</Link>
             <Link href={'/contact'}  className='text-2xl font-medium text-black'>Contact</Link>
         </ul>
