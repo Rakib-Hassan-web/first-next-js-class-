8dbb8a6cd03da74ee39e8cb09cb6d5287d1a6b2a
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-20 11:51:01 +0600

    about page com

--- app/about/page.js
+++ app/about/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='bg-purple-400 h-screen w-full  flex justify-center items-center text-5xl text-black '>This is About Page</div>
+    <div className='bg-purple-400 h-screen w-full  flex justify-center items-center text-5xl text-black font-bold '>This is About Page</div>
   )
 }
 
